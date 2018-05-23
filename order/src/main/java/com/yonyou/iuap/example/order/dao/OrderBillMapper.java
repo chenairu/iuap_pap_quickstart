5e8d71f01428576c09ca84acbbcd33fa9844df0f
Aton <Aton@Aton-618>
2018-05-23 15:55:14 +0800

    订单管理：主子表示例

--- order/src/main/java/com/yonyou/iuap/example/order/dao/OrderBillMapper.java
+++ order/src/main/java/com/yonyou/iuap/example/order/dao/OrderBillMapper.java
@@ -1,23 +1,27 @@
 package com.yonyou.iuap.example.order.dao;
 
+import java.util.List;
 import java.util.Map;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.data.domain.PageRequest;
 
+import com.yonyou.iuap.example.common.dao.GenericMapper;
 import com.yonyou.iuap.example.order.entity.OrderBill;
 import com.yonyou.iuap.mvc.type.SearchParams;
 import com.yonyou.iuap.mybatis.type.PageResult;
 import com.yonyou.iuap.persistence.mybatis.anotation.MyBatisRepository;
 
 @MyBatisRepository
-public interface OrderBillMapper{
+public interface OrderBillMapper extends GenericMapper<OrderBill>{
 
-	PageResult<OrderBill> selectAllByPage(@Param("page") PageRequest pageRequest, @Param("search") SearchParams searchParams);
+	//public PageResult<OrderBill> selectAllByPage(@Param("page") PageRequest pageRequest, @Param("search") SearchParams searchParams);
 	
-	int insert(OrderBill entity);
+	//public List<OrderBill> queryList(@Param("condition")Map<String,Object> params);
+	
+	//public int insert(OrderBill entity);
 
-	int update(OrderBill entity);
+	//public int update(OrderBill entity);
 
-	int delete(@Param("condition")Map<String,Object> params);
+	//public int delete(@Param("condition")Map<String,Object> params);
 	
 }
--- order/src/main/java/com/yonyou/iuap/example/order/dao/OrderDetailMapper.java
+++ order/src/main/java/com/yonyou/iuap/example/order/dao/OrderDetailMapper.java
@@ -1,5 +1,6 @@
 package com.yonyou.iuap.example.order.dao;
 
+import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.annotations.Param;
@@ -13,12 +14,14 @@ import com.yonyou.iuap.persistence.mybatis.anotation.MyBatisRepository;
 @MyBatisRepository
 public interface OrderDetailMapper{
 
-	PageResult<OrderDetail> selectAllByPage(@Param("page") PageRequest pageRequest, @Param("search") SearchParams searchParams);
+	public PageResult<OrderDetail> selectAllByPage(@Param("page") PageRequest pageRequest, @Param("condition") SearchParams searchParams);
 	
-	int insert(OrderDetail entity);
+	public List<OrderDetail> queryList(@Param("condition") Map<String,Object> params);
+	
+	public int insert(OrderDetail entity);
 
-	int update(OrderDetail entity);
+	public int update(OrderDetail entity);
 	
-	int delete(@Param("condition")Map<String,Object> params);
+	public int delete(@Param("condition")Map<String,Object> params);
 	
 }
