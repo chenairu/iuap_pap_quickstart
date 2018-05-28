3021bdf8bd99ef4a8971ff11d2b932bb530e9ffc
Aton <Aton@Aton-618>
2018-05-28 08:35:11 +0800

    工单流程示例

---
+++ src/main/webapp/pages/workorder/meta.js
@@ -0,0 +1,26 @@
+var meta = {
+
+	meta : {
+		id : {},
+		code : {
+			required : true,
+			nullMsg : "不能为空"
+		},
+		name : {
+			required : true,
+			nullMsg : "不能为空"
+		},
+		type : {
+			required : true,
+			nullMsg : "不能为空"
+		},
+		status : {},
+		remark : {},
+		version : {},
+		createTime : {},
+		createUser : {},
+		lastModified : {},
+		lastModifyUser : {}
+	}
+
+};
