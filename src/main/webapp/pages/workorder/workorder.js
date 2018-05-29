1396d0d8a1941fa138acbed71fb4fc5f32855a81
Aton <Aton@Aton-618>
2018-05-29 20:45:55 +0800

    增加申请人、申请时间、工单内容、完成时间

--- src/main/webapp/pages/workorder/workorder.js
+++ src/main/webapp/pages/workorder/workorder.js
@@ -408,7 +408,6 @@ define(['text!./workorder.html',
                     viewModel.formData.setRowSelect(0);
                     
                     //设置业务操作逻辑
-                    //viewModel.event.clearDa(viewModel.subGridData);
                     var row = viewModel.formData.getCurrentRow();
                     
                     //显示操作卡片
@@ -420,9 +419,35 @@ define(['text!./workorder.html',
                     viewModel.event.initTableHeight();
                 },
                 
+                viewClick: function(){
+        			var selectArray = viewModel.gridData.selectedIndices();
+        			if (selectArray.length < 1) {
+        				u.messageDialog({
+        	            	msg: "请选择要提交的记录!",
+        	            	title: "提示",
+        	            	btnText: "OK"
+        				});
+        				return;
+        			}
+
+        	        if (selectArray.length > 1) {
+        	        	u.messageDialog({
+        	            	msg: "一次只能查看一条记录!",
+        	            	title: "提示",
+        	            	btnText: "OK"
+        	        	});
+        	        	return;
+        	        }
+
+        	        var selectedData = viewModel.gridData.getSimpleData({
+        	        	type: "select"
+        	        });
+
+        	        viewModel.flowEvent.doView(selectedData[0]);
+                },
+                
                 //主表行——双击事件
                 dbClickRow:function(gridObj){
-                    //viewModel.event.doEdit(gridObj.rowObj.value);
                 	viewModel.flowEvent.doView(gridObj.rowObj.value);
                 },
                 
