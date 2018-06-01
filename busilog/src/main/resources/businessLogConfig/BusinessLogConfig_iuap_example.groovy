7b01f1cf3f8521eb659a89a94924163cfeac9fb1
Aton <Aton@Aton-618>
2018-06-01 08:42:29 +0800

    业务日志示例

---
+++ busilog/src/main/resources/businessLogConfig/BusinessLogConfig_iuap_example.groovy
@@ -0,0 +1,9 @@
+import static com.yonyou.uap.ieop.busilog.context.ContextKeyConstant.BUSINESS_SYS_ID;
+class BusinessLogConfig_iuap_example {
+
+    def context;    
+    def workorder_info_save() {
+        [category:"业务日志",log:"工单申请流程：执行提交方法:IP地址为${context._ip},USER用户为${context._user},TIME操作时间为${context._time}"]
+    }
+    
+}
