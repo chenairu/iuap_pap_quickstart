d012e02661fb5c986812922fb0917c48db0062ad
Aton <Aton@192.168.128.1>
2018-06-01 12:00:03 +0800

    本地合并

--- src/main/webapp/pages/template/template.js
+++ src/main/webapp/pages/template/template.js
@@ -20,46 +20,46 @@ define(['text!./template.html',
             getUrl = ctx + "/get",
                 window.csrfDefense();									//跨站请求伪造防御
             $(element).html(template);
-            event.formDivShow(false);
-            event.pageinit(element);
+            viewModel.event.formDivShow(false);
+            viewModel.event.pageinit(element);
         }
-        event = {
+        viewModel.event = {
             pageinit: function (element) {
                 viewModel.app = u.createApp({
                     el: element,
                     model: viewModel
                 });
                 var paginationDiv = $(element).find("#pagination")[0];
-                event.comps = new u.pagination({
+                viewModel.event.comps = new u.pagination({
                     el: paginationDiv,
                     jumppage: true
                 });
-                event.comps.update({
+                viewModel.event.comps.update({
                     pageSize: 5
                 }); //默认每页显示5条数据
-                event.initGridDataList();
-                event.pageChange();
-                event.sizeChange();
+                viewModel.event.initGridDataList();
+                viewModel.event.pageChange();
+                viewModel.event.sizeChange();
                 $(".search-enter").keydown(function (e) {
                     if (e.keyCode == 13) {
                         $("#user-action-search").trigger("click");
-                        u.stopEvent(e);
+                        u.stopviewModel.event(e);
                     }
                 });
             },
             //换页
             pageChange: function () {
-                event.comps.on("pageChange", function (pageIndex) {
+                viewModel.event.comps.on("pageChange", function (pageIndex) {
                     viewModel.draw = pageIndex + 1;
-                    event.initGridDataList();
+                    viewModel.event.initGridDataList();
                 });
             },
             //换每页显示数量
             sizeChange: function (size) {
-                event.comps.on("sizeChange", function (arg) {
+                viewModel.event.comps.on("sizeChange", function (arg) {
                     viewModel.pageSize = parseInt(arg);
                     viewModel.draw = 1;
-                    event.initGridDataList();
+                    viewModel.event.initGridDataList();
                 });
             },
             //grid初始化
@@ -84,13 +84,13 @@ define(['text!./template.html',
                         if ((res && res.success == "success", res.detailMsg.data)) {
                             var totalCount = res.detailMsg.data.totalElements;
                             var totalPage = res.detailMsg.data.totalPages;
-                            event.comps.update({
+                            viewModel.event.comps.update({
                                 totalPages: totalPage,
                                 pageSize: viewModel.pageSize,
                                 currentPage: viewModel.draw,
                                 totalCount: totalCount
                             });
-                            event.clearDt(viewModel.gridData);
+                            viewModel.event.clearDt(viewModel.gridData);
                             viewModel.gridData.setSimpleData(res.detailMsg.data.content, {
                                 unSelect: true
                             });
@@ -117,7 +117,7 @@ define(['text!./template.html',
                     viewModel.formData.removeAllRows();
                     viewModel.formData.createEmptyRow();
                 }
-                event.formDivShow(true);
+                viewModel.event.formDivShow(true);
             },
 
             //保存按钮点击
@@ -126,7 +126,7 @@ define(['text!./template.html',
                     u.messageDialog(mustTips);
                     return;
                 }
-                event.addNewData();
+                viewModel.event.addNewData();
             },
 
             //新增数据
@@ -137,13 +137,13 @@ define(['text!./template.html',
                         delete data.operate;
                     }
                 }
-                var jsonData = event.genDataList(data);
+                var jsonData = viewModel.event.genDataList(data);
                 $ajax(
                     saveRowUrl,
                     JSON.stringify(jsonData),
                     function (res) {
                         if (res && res.success == "success") {
-                            event.initGridDataList();
+                            viewModel.event.initGridDataList();
                         } else {
                             u.messageDialog({
                                 msg: res.message,
@@ -160,7 +160,7 @@ define(['text!./template.html',
 
             //取消按钮点击
             cancelClick: function () {
-                event.formDivShow(false);
+                viewModel.event.formDivShow(false);
             },
 
             //查询按钮点击
@@ -173,7 +173,7 @@ define(['text!./template.html',
                         queryData[this.name] = removeSpace(this.value);
                     });
                 viewModel.gridData.addParams(queryData);
-                event.initGridDataList();
+                viewModel.event.initGridDataList();
             },
             //清空查询条件
             cleanSearch: function () {
@@ -205,7 +205,7 @@ define(['text!./template.html',
                     var seldatas = viewModel.gridData.getSimpleData({
                         type: "select"
                     });
-                    event.del(seldatas);
+                    viewModel.event.del(seldatas);
                 }
             },
 
@@ -221,7 +221,7 @@ define(['text!./template.html',
                             type: "current"
                         });
                         console.log("del:", seldatas);
-                        event.del(seldatas);
+                        viewModel.event.del(seldatas);
                     }
                 });
             },
@@ -239,7 +239,7 @@ define(['text!./template.html',
                     JSON.stringify(arr),
                     function (res) {
                         if (res && res.success == "success") {
-                            event.initGridDataList();
+                            viewModel.event.initGridDataList();
                         } else {
                             var msg = "";
                             for (var key in res.detailMsg) {
@@ -305,9 +305,9 @@ define(['text!./template.html',
             optFun: function (obj) {
                 var dataTableRowId = obj.row.value["$_#_@_id"];
                 var delfun =
-                    "data-bind=click:event.delRow.bind($data," + dataTableRowId + ")";
+                    "data-bind=click:viewModel.event.delRow.bind($data," + dataTableRowId + ")";
                 var editfun =
-                    "data-bind=click:event.btnAddClicked.bind($data," +
+                    "data-bind=click:viewModel.event.btnAddClicked.bind($data," +
                     dataTableRowId +
                     ")";
                 obj.element.innerHTML =
@@ -378,7 +378,7 @@ define(['text!./template.html',
                 $("#fileName").remove();
                 $("#dialog_content").find(".choosefile").append('<input class="u-input" style="display: none;text-align: center;"  type="file" name="fileName" id="fileName"/>');
                 var demoInput = document.getElementById('fileName');
-                demoInput.addEventListener('change', event.onUploadExcel);
+                demoInput.addviewModel.eventListener('change', viewModel.event.onUploadExcel);
             },
     
             onUploadExcel: function () {
@@ -387,15 +387,15 @@ define(['text!./template.html',
                 console.log("filevalue:", filevalue);
                 if (!filevalue || filevalue.length < 1) {
                     var demoInput = document.getElementById('fileName');
-                    demoInput.removeEventListener('change', event.onUploadExcel);
-                    demoInput.addEventListener('change', event.onUploadExcel);
+                    demoInput.removeviewModel.eventListener('change', viewModel.event.onUploadExcel);
+                    demoInput.addviewModel.eventListener('change', viewModel.event.onUploadExcel);
                     return;
                 } else {
                     $("#filenamediv2").html(filevalue[0].name);
                 }
     
     
-                event.uploadingshow();
+                viewModel.event.uploadingshow();
                 $.ajaxFileUpload({
                     url: ctx + "/excelDataImport",
                     secureuri: false,
@@ -406,22 +406,22 @@ define(['text!./template.html',
                         $("#fileName").remove();
                         $("#dialog_content").find(".choosefile").append('<input class="u-input" style="display: none; text-align: center;"  type="file" name="fileName" id="fileName"/>');
                         var demoInput = document.getElementById('fileName');
-                        demoInput.removeEventListener('change', event.onUploadExcel);
-                        demoInput.addEventListener('change', event.onUploadExcel);
+                        demoInput.removeviewModel.eventListener('change', viewModel.event.onUploadExcel);
+                        demoInput.addviewModel.eventListener('change', viewModel.event.onUploadExcel);
                         window.clearInterval(viewModel.loadingTimer);
                         $('.file-lodedPart').width(200);
-                        event.uploadingHide();
+                        viewModel.event.uploadingHide();
                         $("#uploadingIMG2").attr("src", "../example/static/success.svg");
                         $("#uploadingMsg2").html("导入成功").addClass("success").removeClass("uploading").removeClass("fail");
-                        event.initGridDataList();
+                        viewModel.event.initGridDataList();
                         md.close();
                     },
                     error: function (XMLHttpRequest, textStatus, errorThrown) {
                         $("#fileName").remove();
                         $("#dialog_content").find(".choosefile").append('<input class="u-input" style="display: none;text-align: center;"  type="file" name="fileName" id="fileName"/>');
                         var demoInput = document.getElementById('fileName');
-                        demoInput.removeEventListener('change', event.onUploadExcel);
-                        demoInput.addEventListener('change', event.onUploadExcel);
+                        demoInput.removeviewModel.eventListener('change', viewModel.event.onUploadExcel);
+                        demoInput.addviewModel.eventListener('change', viewModel.event.onUploadExcel);
                         $("#uploadingIMG2").attr("src", "../example/static/fail.svg");
                         $("#uploadingMsg2").html("导入失败" + data.message).addClass("fail").removeClass("uploading").removeClass("success");
                         window.clearInterval(viewModel.loadingTimer);
