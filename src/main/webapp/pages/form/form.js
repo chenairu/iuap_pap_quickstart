672138c883f8497393a62363d999a72c49f24d1f
rfdeval <rfdeval@gmail.com>
2018-06-22 06:31:34 +0800

    综合表单示例初稿

--- src/main/webapp/pages/form/form.js
+++ src/main/webapp/pages/form/form.js
@@ -5,22 +5,39 @@ define(['text!./form.html',
     "../../utils/utils.js",
     "../../utils/ajax.js",
     "../../utils/tips.js",
-    "./viewModel.js"], function (template, cookie) {
-        var ctx, listRowUrl, saveRowUrl, delRowUrl, getUrl, element;
-        function init(element, cookie) {
+    "./viewModel.js"], function (template) {
+        var element;
+        function init(element) {
             element = element;
             $(element).html(template);
-            ctx = cookie.appCtx + "/example_dictionary";
-            listRowUrl = ctx + "/list"; //列表查询URL
-            saveRowUrl = ctx + "/save"; //新增和修改URL， 有id为修改 无id为新增
-            delRowUrl = ctx + "/delete"; //刪除URL
-            getUrl = ctx + "/get",
-            window.csrfDefense();									//跨站请求伪造防御
-            $(element).html(template);
-
+            viewModel.event.pageinit(element);
+            //撑满高度布局
+            $("#myLayout").height(document.body.scrollHeight);
         }
         viewModel.event = {
+            pageinit: function (element) {
+                viewModel.app = u.createApp({
+                    el: element,
+                    model: viewModel
+                });
+                viewModel.event.initForm();//初始化表单
+            },
 
+            initForm: function () {
+                var r1 = viewModel.formData1.createEmptyRow();
+                r1.setValue('sys', "否");
+                viewModel.formData1.setRowSelect(0);
+                viewModel.formData1.on("sys.valueChange", function (e) {
+                    alert(e.newValue);
+                });
+
+                var r2 = viewModel.formData2.createEmptyRow();
+                r2.setValue('sex', "男");
+                viewModel.formData2.setRowSelect(0);
+                viewModel.formData2.on("sex.valueChange", function (e) {
+                    alert(e.newValue);
+                });
+            }
         }
         return {
             template: template,
--- src/main/webapp/pages/form/viewModel.js
+++ src/main/webapp/pages/form/viewModel.js
@@ -1,14 +1,25 @@
-var meta = {
+var meta1 = {
   meta: {
-    id: {},
     code: {},
-    name: {},
+    createtime: {},
+    sys: {
+      type: "string",
+      default: "否"
+    }
   }
 };
+
+var meta2 = {
+  meta: {
+    sex: {}
+  }
+};
+
+
+//主要为了测试2个formdata中分别放置下拉框，默认值和监控失效的问题。
 var viewModel = {
-  draw: 1,
-  pageSize: 5,
-  gridData: new u.DataTable(meta),
-  formData: new u.DataTable(meta),
-  // yesOrNo: [{name: "是", value: "是"}, {name: "否", value: "否"}]
+  formData1: new u.DataTable(meta1),
+  formData2: new u.DataTable(meta2),
+  com1: [{ name: "是", value: "是" }, { name: "否", value: "否" }],
+  com2: [{ name: "男", value: "男" }, { name: "女", value: "女" }]
 };
