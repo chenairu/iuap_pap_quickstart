115f5a0ff8d7ec5694264fc70032b8dc6a17f5b0
'xiadl' <'xiadlc@yonyou.com'>
2018-07-05 16:07:43 +0800

    附件上传调整

--- sanyorder/src/main/java/com/yonyou/iuap/example/sanyorder/service/SanyOrderService.java
+++ sanyorder/src/main/java/com/yonyou/iuap/example/sanyorder/service/SanyOrderService.java
@@ -1,15 +1,21 @@
 package com.yonyou.iuap.example.sanyorder.service;
 
 import java.util.Date;
+import java.util.List;
 import java.util.Random;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import com.yonyou.iuap.baseservice.bpm.service.GenericBpmService;
+import com.yonyou.iuap.example.sanyorder.dao.SanyOrderAttachmentMapper;
 import com.yonyou.iuap.example.sanyorder.dao.SanyOrderMapper;
+import com.yonyou.iuap.example.sanyorder.entity.AttachmentEntity;
 import com.yonyou.iuap.example.sanyorder.entity.SanyOrder;
+import com.yonyou.iuap.mvc.type.SearchParams;
+import com.yonyou.iuap.mybatis.type.PageResult;
 
 import yonyou.bpm.rest.ex.util.DateUtil;
 
@@ -29,12 +35,52 @@ public class SanyOrderService extends GenericBpmService<SanyOrder>{
 		return super.insert(SanyOrder);
 	}
 	
+	public SanyOrder saveWithAttachment(SanyOrder SanyOrder) {
+		if(SanyOrder.getId() == null){
+			SanyOrder insertSanyOrder = new SanyOrder();
+			insertSanyOrder = super.insert(SanyOrder);
+			String id = insertSanyOrder.getId();
+			String name = insertSanyOrder.getOrderName();
+			List<AttachmentEntity> attachments = SanyOrder.getAttachment();
+			for(AttachmentEntity att:attachments){
+				att.setRefId(id);
+				att.setRefName(name);
+				sanyOrderAttachmentMapper.insert(att);
+			}
+			return insertSanyOrder;
+		}else{
+			super.update(SanyOrder);
+			String id = SanyOrder.getId();
+			String name = SanyOrder.getOrderName();
+			List<AttachmentEntity> attachments = SanyOrder.getAttachment();
+			for(AttachmentEntity att:attachments){
+				att.setRefId(id);
+				att.setRefName(name);
+				sanyOrderAttachmentMapper.insert(att);
+			}
+			return SanyOrder;
+		}
+	}
+	
+	public Page<SanyOrder> getListWithAttach(PageRequest pageRequest, SearchParams searchParams) {
+		Page<SanyOrder> page = sanyOrderMapper.selectAllByPage(pageRequest, searchParams).getPage();
+		List<SanyOrder> listSanyOrder = page.getContent();
+		for(SanyOrder sanyOrder : listSanyOrder){
+			List<AttachmentEntity> attachments = sanyOrderAttachmentMapper.getRefId(sanyOrder.getId());
+			sanyOrder.setAttachment(attachments);
+		}
+		return page;
+	}
 
 	private SanyOrderMapper sanyOrderMapper;
+	
+	@Autowired
+	private SanyOrderAttachmentMapper sanyOrderAttachmentMapper;
 
 	@Autowired
 	public void setSanyOrderMapper(SanyOrderMapper sanyOrderMapper) {
 		this.sanyOrderMapper = sanyOrderMapper;
 		super.setIbatisMapperEx(this.sanyOrderMapper);
 	}
+
 }
--- sanyorder/src/main/java/com/yonyou/iuap/example/sanyorder/web/controller/SanyOrderController.java
+++ sanyorder/src/main/java/com/yonyou/iuap/example/sanyorder/web/controller/SanyOrderController.java
@@ -1,12 +1,12 @@
 package com.yonyou.iuap.example.sanyorder.web.controller;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,9 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.yonyou.iuap.baseservice.bpm.controller.GenericBpmController;
-import com.yonyou.iuap.example.sanyorder.entity.AttachmentEntity;
 import com.yonyou.iuap.example.sanyorder.entity.SanyOrder;
-import com.yonyou.iuap.example.sanyorder.service.SanyOrderAttachmentService;
 import com.yonyou.iuap.example.sanyorder.service.SanyOrderService;
 import com.yonyou.iuap.mvc.annotation.FrontModelExchange;
 import com.yonyou.iuap.mvc.type.SearchParams;
@@ -29,30 +27,19 @@ public class SanyOrderController extends GenericBpmController<SanyOrder> {
 
 	private SanyOrderService sanyOrderService;
 	
-	@Autowired
-	private SanyOrderAttachmentService SanyOrderAttachmentService;
-	
-	@RequestMapping(value = "/saveToFile", method = RequestMethod.POST)
+	@RequestMapping(value = "/saveWithAttach", method = RequestMethod.POST)
 	public Object setFileBill(@RequestBody SanyOrder entity){
-		SanyOrder sanyOrder = sanyOrderService.save(entity);
-		String id = sanyOrder.getId();
-		String name = sanyOrder.getOrderName();
-		List<AttachmentEntity> attachments = entity.getAttachment();
-		for(AttachmentEntity att:attachments){
-			att.setRefId(id);
-			att.setRefName(name);
-			SanyOrderAttachmentService.save(att);
-		}
-		return this.buildSuccess(entity);
+		SanyOrder sanyOrder = sanyOrderService.saveWithAttachment(entity);
+		return this.buildSuccess(sanyOrder);
 	}
 	
-	@RequestMapping(value = "/getToFile", method = RequestMethod.GET)
-	public Object getFileBill(PageRequest pageRequest, SearchParams searchParams){
-		SanyOrder sanyOrder = (SanyOrder) super.get(pageRequest,searchParams);
-		
-		List<AttachmentEntity> atts = SanyOrderAttachmentService.getRefId(sanyOrder.getId());
-		sanyOrder.setAttachment(atts);
-		return this.buildSuccess(sanyOrder);
+	@RequestMapping(value = "/getListWithAttach", method = RequestMethod.GET)
+	public Object getFileBill(PageRequest pageRequest,
+			   @FrontModelExchange(modelType = SanyOrder.class) SearchParams searchParams){
+		Page<SanyOrder> page = sanyOrderService.getListWithAttach(pageRequest,searchParams);
+		Map<String, Object> map = new HashMap<String, Object>();
+		map.put("data", page);
+		return this.buildMapSuccess(map);
 	}
 	
 	@Autowired
