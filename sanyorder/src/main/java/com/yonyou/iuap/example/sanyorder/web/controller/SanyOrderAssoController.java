c6c6a2fae2226039a5da5d86a90faeab0a8f4370
yangliangs <yangliangs@yonyou.com>
2018-07-12 15:05:48 +0800

    finish asso api getAssoVo

--- sanyorder/src/main/java/com/yonyou/iuap/example/sanyorder/web/controller/SanyOrderAssoController.java
+++ sanyorder/src/main/java/com/yonyou/iuap/example/sanyorder/web/controller/SanyOrderAssoController.java
@@ -1,39 +1,41 @@
 package com.yonyou.iuap.example.sanyorder.web.controller;
 
-import com.yonyou.iuap.base.web.BaseController;
-import com.yonyou.iuap.example.sanyorder.entity.SanyOrderAssoVo;
+import com.yonyou.iuap.baseservice.controller.GenericAssoController;
+import com.yonyou.iuap.example.sanyorder.entity.SanyOrder;
+import com.yonyou.iuap.example.sanyorder.entity.SanyOrderContract;
 import com.yonyou.iuap.example.sanyorder.service.SanyOrderContractService;
 import com.yonyou.iuap.example.sanyorder.service.SanyOrderService;
-import com.yonyou.iuap.mvc.type.SearchParams;
-import org.apache.commons.collections.MapUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 @Controller
 @RequestMapping(value = "/sany_order")
-public class SanyOrderAssoController extends BaseController {
+public class SanyOrderAssoController extends GenericAssoController<SanyOrder> {
 
-    @Autowired
-    SanyOrderService sanyOrderService;
-    @Autowired
-    SanyOrderContractService sanyOrderContractService;
+    private SanyOrderService sanyOrderService;
+    private SanyOrderContractService sanyOrderContractService;
 
-    @RequestMapping(value = "/getAssoVo")
-    @ResponseBody
-    public Object  getAssoVo(PageRequest pageRequest,
-            SearchParams searchParams){
-        String id = MapUtils.getString(searchParams.getSearchMap(), "id");
-        SanyOrderAssoVo vo = new SanyOrderAssoVo();
-        if (null==id){ return buildSuccess();}
-        vo.setSanyOrder( sanyOrderService.findById(id)  );//刷数据,关键是TS
-        List contractList= sanyOrderContractService.queryList("orderId",id);
-        vo.setSanyOrderContractList(contractList);
-        return this.buildSuccess(vo) ;
+
+    /**
+     * 注入主表service
+     * @param sanyOrderService
+     */
+    @Autowired
+    public void setSanyOrderService(SanyOrderService sanyOrderService) {
+        this.sanyOrderService = sanyOrderService;
+        super.setService(sanyOrderService);
     }
+
+    /**
+     * 注入子表service
+     * @param sanyOrderContractService
+     */
+    @Autowired
+    public void setSanyOrderService(SanyOrderContractService sanyOrderContractService) {
+        this.sanyOrderContractService = sanyOrderContractService;
+        super.setSubService(SanyOrderContract.class,sanyOrderContractService);
+    }
+
 
 }
