e7638de274d31fef5a6e11ece5a26d1ae27a6035
yangliangs <yangliangs@yonyou.com>
2018-07-19 14:29:48 +0800

    asso controller goback to baseService

--- showoff/src/main/java/com/yonyou/iuap/example/controller/ShowOffAssoController.java
+++ showoff/src/main/java/com/yonyou/iuap/example/controller/ShowOffAssoController.java
@@ -1,14 +1,12 @@
 package com.yonyou.iuap.example.controller;
 
-import com.yonyou.iuap.baseservice.vo.GenericAssoVo;
+import com.yonyou.iuap.baseservice.controller.GenericAssoController;
 import com.yonyou.iuap.example.entity.ShowOff;
 import com.yonyou.iuap.example.entity.ShowOffDetail;
 import com.yonyou.iuap.example.service.ShowOffDetailService;
 import com.yonyou.iuap.example.service.ShowOffService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -36,11 +34,6 @@ public class ShowOffAssoController extends GenericAssoController<ShowOff> {
     public void setDetailService(ShowOffDetailService subService) {
         this.sanyOrderDetailService = subService;
         super.setSubService(ShowOffDetail.class,subService);
-    }
-
-    @Override
-    public Object  saveAssoVo(@RequestBody GenericAssoVo<ShowOff> vo){
-        return super.saveAssoVo(vo) ;
     }
 
 
