a5a79a15423f6db1fbb92d76d3d471b1fed7881c
shixtf <shixtf@yonyou.com>
2018-07-26 15:02:01 +0800

    修改sanservice

--- sanyorder/src/main/java/com/yonyou/iuap/example/sanyorder/service/SanyOrderService.java
+++ sanyorder/src/main/java/com/yonyou/iuap/example/sanyorder/service/SanyOrderService.java
@@ -57,4 +57,41 @@ public class SanyOrderService extends GenericBpmService<SanyOrder>{
         return bpmform;
     }
 
+    @Override
+    public BPMFormJSON buildBPMFormJSON(String processDefineCode, SanyOrder entity) {
+        try {
+            BPMFormJSON bpmform = new BPMFormJSON();
+            bpmform.setProcessDefinitionKey(processDefineCode);
+            String userName = InvocationInfoProxy.getUsername();
+            try {
+                userName = URLDecoder.decode(userName,"utf-8");
+            } catch (UnsupportedEncodingException e) {
+                userName =InvocationInfoProxy.getUsername();
+            }
+            //title
+            String title = userName + "提交的【工单】,单号 是" + entity.getBpmBillCode() + ", 请审批";
+            bpmform.setTitle(title);
+
+            // 单据id
+            bpmform.setFormId(entity.getId().toString());
+            // 单据号
+            bpmform.setBillNo(entity.getBpmBillCode());
+            // 制单人
+            bpmform.setBillMarker(InvocationInfoProxy.getUserid());
+            // 其他变量
+            bpmform.setOtherVariables(buildEntityVars(entity));
+            // 单据url
+            bpmform.setFormUrl("/iuap_pap_quickstart/pages/workorder/workorder.js");	// 单据url
+            // 流程实例名称
+            bpmform.setProcessInstanceName(title);										// 流程实例名称
+            // 流程审批后，执行的业务处理类(controller对应URI前缀)
+            bpmform.setServiceClass("/iuap_pap_quickstart/sany_order");// 流程审批后，执行的业务处理类(controller对应URI前缀)
+            //设置单据打开类型 uui/react
+            bpmform.setFormType(BPMFormJSON.FORMTYPE_REACT);
+            return bpmform;
+        } catch (Exception ex) {
+            throw new RuntimeException(ex.getMessage());
+        }
+    }
+
 }
