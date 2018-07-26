576e2383bdabc8099b05ad1dd2bb0c222504b57c
gsjzhbj <gsjzhbj@sina.com>
2018-07-26 14:40:25 +0800

    三一示例节点参数

--- sanyorder/src/main/java/com/yonyou/iuap/example/sanyorder/service/SanyOrderService.java
+++ sanyorder/src/main/java/com/yonyou/iuap/example/sanyorder/service/SanyOrderService.java
@@ -48,9 +48,9 @@ public class SanyOrderService extends GenericBpmService<SanyOrder>{
         } catch (UnsupportedEncodingException e) {
             userName =InvocationInfoProxy.getUsername();
         }
-        String title = userName + "提交的【工单】,单号 是" + entity.getBpmBillCode() + ", 请审批";
+        String title = userName + "提交的单据,单号 是" + entity.getBpmBillCode() + ", 请审批";
         bpmform.setTitle(title);
-        bpmform.setFormUrl("/iuap_pap_quickstart/pages/workorder/workorder.js");	// 单据url
+        bpmform.setFormUrl("/#/templates/example-edit?btnFlag=2&search_id=${SEARCH_VALUE}");	// 单据url
         bpmform.setProcessInstanceName(title);										// 流程实例名称
         bpmform.setServiceClass("/iuap_pap_quickstart/sany_order");// 流程审批后，执行的业务处理类(controller对应URI前缀)
 
